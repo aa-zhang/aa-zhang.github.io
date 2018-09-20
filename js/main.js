e64215344f4936c4b1eafa1626945c0958bcb105
AaronZhang314 <32945139+AaronZhang314@users.noreply.github.com>
2018-09-20 19:39:15 -0400

    Animation Tweaks

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
 
-
 window.onscroll = function() {navStick()};
 
 var sideNav = document.getElementById("sideNav");
@@ -21,19 +20,3 @@ function navStick() {
   }
 }
 
-
-document.getElementById ("openNav").addEventListener ("click", sideOpen, false);
-document.getElementById ("closeNav").addEventListener ("click", sideClose, false);
-function sideOpen() {
-    document.getElementById("sideNav").style.display = "inline";
-    document.getElementById("closeNav").style.display = "inline";
-    document.getElementsByClass("sticky").style.display = "inline";
-    document.getElementById("openNav").style.display = "none";
-}
-
-function sideClose() {
-    document.getElementById("sideNav").style.display = "none";
-    document.getElementById("closeNav").style.display = "none";
-    document.getElementsByClass("sticky").style.display = "none";
-    document.getElementById("openNav").style.display = "inline";
-}
