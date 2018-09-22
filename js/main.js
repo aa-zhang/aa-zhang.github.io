35760b61feacab480d11062ebd19d298d2e77ab7
AaronZhang314 <32945139+AaronZhang314@users.noreply.github.com>
2018-09-21 20:51:26 -0400

    Added horizontal rules

--- js/main.js
+++ js/main.js
@@ -2,21 +2,3 @@ window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
 
-window.onscroll = function() {navStick()};
-
-var sideNav = document.getElementById("sideNav");
-var openNav = document.getElementById("openNav");
-var body = document.getElementById("mainBody");
-var sticky = body.offsetTop;
-
-function navStick() {
-  if (window.pageYOffset >= sticky) {
-    sideNav.classList.add("sticky");
-    openNav.classList.add("sticky");
-  } 
-  else {
-    sideNav.classList.remove("sticky");
-    openNav.classList.remove("sticky");
-  }
-}
-
